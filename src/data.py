38ebf84f578afef0e3976ad2b78ee2fb7a2b3b63
ds-wook <leewook94@gmail.com>
2024-01-18 18:53:21 +0900

    Update!

--- src/data.py
+++ src/data.py
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore")
 
 
 class Warehouse:
-    root = "kaggle/input/predict-energy-behavior-of-prosumers"
+    root = "/kaggle/input/predict-energy-behavior-of-prosumers"
 
     data_columns = [
         "target",
