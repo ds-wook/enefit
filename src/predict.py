b666d8e3ca72ac3c95dcae4652071e0ffc233653
ds-wook <leewook94@gmail.com>
2024-01-23 21:30:31 +0900

    Update!

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,9 @@
-import os
 from pathlib import Path
 
 import hydra
 import joblib
 import numpy as np
 import pandas as pd
-from cuml import ForestInference
 from omegaconf import DictConfig
 from sklearn.ensemble import VotingRegressor
 
@@ -19,38 +17,21 @@ except ModuleNotFoundError:
     raise ModuleNotFoundError("Please install enefit package from")
 
 
-def fast_inference(voting_regressor: VotingRegressor) -> list[ForestInference]:
-    models = []
-    for model in voting_regressor.estimators_:
-        model.booster_.save_model("tmp.txt")
-        model = ForestInference.load("tmp.txt", model_type="lightgbm")
-        os.remove("tmp.txt")
-        models.append(model)
-
-    return models
-
-
 def predict_model(
-    df_features: pd.DataFrame,
-    hours_lag: int,
-    model_consumptions: list[ForestInference],
-    model_productions: list[ForestInference],
+    df_features: pd.DataFrame, hours_lag: int, model_consumption: VotingRegressor, model_production: VotingRegressor
 ) -> np.ndarray:
     predictions = np.zeros(len(df_features))
 
     mask = df_features["is_consumption"] == 1
-
     predictions[mask.values] = np.clip(
-        df_features[mask][f"target_{hours_lag}h"].fillna(0).values
-        + np.median([model_consumption.predict(df_features[mask]) for model_consumption in model_consumptions], axis=0),
+        df_features[mask][f"target_{hours_lag}h"].fillna(0).values + model_consumption.predict(df_features[mask]),
         0,
         np.inf,
     )
 
     mask = df_features["is_consumption"] == 0
     predictions[mask.values] = np.clip(
-        df_features[mask][f"target_{hours_lag}h"].fillna(0).values
-        + np.median([model_production.predict(df_features[mask]) for model_production in model_productions], axis=0),
+        df_features[mask][f"target_{hours_lag}h"].fillna(0).values + model_production.predict(df_features[mask]),
         0,
         np.inf,
     )
@@ -63,9 +44,7 @@ def _main(cfg: DictConfig):
     env = enefit.make_env()
     iter_test = env.iter_test()
     model_consumption = joblib.load(Path(cfg.models.path) / f"{cfg.models.model_consumption}")
-    model_consumption = fast_inference(model_consumption)
     model_production = joblib.load(Path(cfg.models.path) / f"{cfg.models.model_production}")
-    model_production = fast_inference(model_production)
 
     for (
         df_test,
