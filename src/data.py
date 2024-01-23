ba5323dfac9211d865237767e5d23f552b856074
ds-wook <leewook94@gmail.com>
2024-01-23 17:01:04 +0900

    Update!

--- src/data.py
+++ src/data.py
@@ -10,33 +10,44 @@ warnings.filterwarnings("ignore")
 
 class DataStorage:
     def __init__(self, cfg: DictConfig):
-        self.df_data = pl.read_csv(Path(cfg.data.root) / "train.csv", columns=cfg.data.data_cols, try_parse_dates=True)
+        self.cfg = cfg
+        self.df_data = pl.read_csv(
+            Path(self.cfg.data.root) / "train.csv",
+            columns=self.cfg.data.data_cols,
+            try_parse_dates=True,
+        )
         self.df_client = pl.read_csv(
-            Path(cfg.data.root) / "client.csv", columns=cfg.data.client_cols, try_parse_dates=True
+            Path(self.cfg.data.root) / "client.csv",
+            columns=self.cfg.data.client_cols,
+            try_parse_dates=True,
         )
         self.df_gas_prices = pl.read_csv(
-            Path(cfg.data.root) / "gas_prices.csv", columns=cfg.data.gas_prices_cols, try_parse_dates=True
-        )
-        self.df_forecast_weather = pl.read_csv(
-            Path(cfg.data.root) / "forecast_weather.csv", columns=cfg.data.forecast_weather_cols, try_parse_dates=True
+            Path(self.cfg.data.root) / "gas_prices.csv",
+            columns=self.cfg.data.gas_prices_cols,
+            try_parse_dates=True,
         )
         self.df_electricity_prices = pl.read_csv(
-            Path(cfg.data.root) / "electricity_prices.csv",
-            columns=cfg.data.electricity_prices_cols,
+            Path(self.cfg.data.root) / "electricity_prices.csv",
+            columns=self.cfg.data.electricity_prices_cols,
+            try_parse_dates=True,
+        )
+        self.df_forecast_weather = pl.read_csv(
+            Path(self.cfg.data.root) / "forecast_weather.csv",
+            columns=self.cfg.data.forecast_weather_cols,
             try_parse_dates=True,
         )
         self.df_historical_weather = pl.read_csv(
-            Path(cfg.data.root) / "historical_weather.csv",
-            columns=cfg.data.historical_weather_cols,
+            Path(self.cfg.data.root) / "historical_weather.csv",
+            columns=self.cfg.data.historical_weather_cols,
             try_parse_dates=True,
         )
         self.df_weather_station_to_county_mapping = pl.read_csv(
-            Path(cfg.data.root) / "weather_station_to_county_mapping.csv",
-            columns=cfg.data.location_cols,
+            Path(self.cfg.data.root) / "weather_station_to_county_mapping.csv",
+            columns=self.cfg.data.location_cols,
             try_parse_dates=True,
         )
         self.df_data = self.df_data.filter(pl.col("datetime") >= pd.to_datetime("2022-01-01"))
-        self.df_target = self.df_data.select(cfg.data.target_cols)
+        self.df_target = self.df_data.select(self.cfg.data.target_cols)
 
         self.schema_data = self.df_data.schema
         self.schema_client = self.df_client.schema
@@ -60,24 +71,24 @@ class DataStorage:
         df_new_historical_weather,
         df_new_target,
     ):
-        df_new_client = pl.from_pandas(df_new_client[self.client_cols], schema_overrides=self.schema_client)
+        df_new_client = pl.from_pandas(df_new_client[self.cfg.client_cols], schema_overrides=self.schema_client)
         df_new_gas_prices = pl.from_pandas(
-            df_new_gas_prices[self.gas_prices_cols],
+            df_new_gas_prices[self.cfg.gas_prices_cols],
             schema_overrides=self.schema_gas_prices,
         )
         df_new_electricity_prices = pl.from_pandas(
-            df_new_electricity_prices[self.electricity_prices_cols],
+            df_new_electricity_prices[self.cfg.electricity_prices_cols],
             schema_overrides=self.schema_electricity_prices,
         )
         df_new_forecast_weather = pl.from_pandas(
-            df_new_forecast_weather[self.forecast_weather_cols],
+            df_new_forecast_weather[self.cfg.forecast_weather_cols],
             schema_overrides=self.schema_forecast_weather,
         )
         df_new_historical_weather = pl.from_pandas(
-            df_new_historical_weather[self.historical_weather_cols],
+            df_new_historical_weather[self.cfg.historical_weather_cols],
             schema_overrides=self.schema_historical_weather,
         )
-        df_new_target = pl.from_pandas(df_new_target[self.target_cols], schema_overrides=self.schema_target)
+        df_new_target = pl.from_pandas(df_new_target[self.cfg.target_cols], schema_overrides=self.schema_target)
 
         self.df_client = pl.concat([self.df_client, df_new_client]).unique(
             ["date", "county", "is_business", "product_type"]
@@ -98,5 +109,5 @@ class DataStorage:
 
     def preprocess_test(self, df_test):
         df_test = df_test.rename(columns={"prediction_datetime": "datetime"})
-        df_test = pl.from_pandas(df_test[self.data_cols[1:]], schema_overrides=self.schema_data)
+        df_test = pl.from_pandas(df_test[self.cfg.data_cols[1:]], schema_overrides=self.schema_data)
         return df_test
